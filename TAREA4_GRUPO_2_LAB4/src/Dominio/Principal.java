2fe69c5ab7235c8c9585b72b8b703f437fd6f9a9
orianagarcia <orugarcia@gmail.com>
2020-05-05 19:11:31 -0300

    lista

--- TAREA4_GRUPO_2_LAB4/src/Dominio/Principal.java
+++ TAREA4_GRUPO_2_LAB4/src/Dominio/Principal.java
@@ -1,13 +1,18 @@
 package Dominio;
 
+import javax.swing.DefaultListModel;
+
 import Ventanas.VentanaPrincipal;
 
 public class Principal {
-
+	private static DefaultListModel<Pelicula> listaPeliculas;
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		VentanaPrincipal v0= new VentanaPrincipal();
 		v0.setVisible(true);
+		listaPeliculas = new DefaultListModel<Pelicula>();
+		
 	}
 
 }
