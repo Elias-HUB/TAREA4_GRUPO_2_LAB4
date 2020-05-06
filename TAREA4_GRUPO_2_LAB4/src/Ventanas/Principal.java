4af74efcb1f66b43b1e16e1ab8e9b54b88a60f0a
Elias <elias.valenzuela@alumnos.frgp.utn.edu.ar>
2020-05-05 23:19:16 -0300

    Se Acomoda el proyecto junto a ventanas
    Se realizan modificaciones con Oriana para un buen funcionamiento de las ventanas

---
+++ TAREA4_GRUPO_2_LAB4/src/Ventanas/Principal.java
@@ -0,0 +1,101 @@
+package Ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Dominio.Pelicula;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPanel;
+	private static DefaultListModel<Pelicula> listModel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					listModel = new DefaultListModel<Pelicula>();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		
+		setTitle("Programa");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnPeliculas = new JMenu("Peliculas");
+		menuBar.add(mnPeliculas);
+		
+		
+		
+		JMenuItem mntmAgregar = new JMenuItem("Agregar");
+		mntmAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//Abre el formulario PanelListadoLibros
+				contentPanel.removeAll();
+				PanelIngresoPeliculas panel = new PanelIngresoPeliculas();
+				//panel.setDefaultListModel(listModel);
+				contentPanel.add(panel);
+				contentPanel.repaint();
+				contentPanel.revalidate();
+			}
+		});
+
+		mnPeliculas.add(mntmAgregar);		
+		
+		JMenuItem mntmListado = new JMenuItem("Listado");
+		mntmListado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//Abre el formulario PanelListadoLibros
+				contentPanel.removeAll();
+				PanelListadoPelicula panel = new PanelListadoPelicula();
+				//panel.setDefaultListModel(listModel);
+				contentPanel.add(panel);
+				contentPanel.repaint();
+				contentPanel.revalidate();				
+			}		
+			
+		});
+		mnPeliculas.add(mntmListado);
+		
+		
+		
+		contentPanel = new JPanel();
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPanel);
+		getContentPane().setLayout(null);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+	}
+}
